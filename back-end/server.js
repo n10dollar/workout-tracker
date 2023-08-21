62dbbaca9a679611914e90c1dfae288999eb8ef3
Neil <ntendolkar@berkeley.edu>
2023-08-21 13:12:12 -0700

    Created app.use(), imported dotenv

--- back-end/server.js
+++ back-end/server.js
@@ -1,14 +1,22 @@
+require('dotenv').config()
+
 const express = require('express')
 
 // express listener
 const app = express()
 
+// middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
 // route handler
 app.get('/', (req, res) => {
-    res.json({message: 'Welcome to the app'})
+    res.json({mssg: 'Welcome to the app'})
 })
 
 // listen for requests
-app.listen(PATH, () => {
-    console.log('listening on port 4000')
+app.listen(process.env.PORT, () => {
+    console.log(`listening on port ${process.env.PORT}`)
 })
